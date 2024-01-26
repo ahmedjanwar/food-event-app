be8b953a6dad0d1da155e01ff422029a2e33b6a6
ahmedjanwar <ajemal840@gmail.com>
2024-01-26 20:12:03 +0200

    backedn update

--- food-event-Backend/models/order.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const orderSchema = new mongoose.Schema({
-  tableId: { type: String, required: true },
-  foodItems: { type: Array, required: true },
-});
-
-module.exports = mongoose.model('Order', orderSchema);
